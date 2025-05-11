6c98c31f8b2da16d32d916f4cf5b111fe1d3d21e
dpdlcl <dpdlcl0111@gmail.com>
2025-05-11 19:19:03 +0900

    feat: solve 2025_0511_even_odd

---
+++ python/2025_0511_even_odd.py
@@ -0,0 +1,29 @@
+"""
+짝수 홀수 개수
+
+정수가 담긴 리스트 num_list가 주어질 때, num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+
+제한사항
+1 ≤ num_list의 길이 ≤ 100
+0 ≤ num_list의 원소 ≤ 1,000
+
+def solution(num_list):
+    answer = []
+    return answer
+"""
+
+def solution(num_list):
+    even = 0
+    odd = 0
+
+    for i in num_list:
+        if i % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+
+    answer = [even, odd]
+    return answer
+
+# test
+print(solution([3, 4, 5, 6, 7]))
